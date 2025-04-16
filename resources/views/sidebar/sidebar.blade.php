047def82c1f2352e8967e9db5f125d5610c8195c
JNyanga <44498304+J-jemoh@users.noreply.github.com>
2025-04-16 16:52:26 +0300

    change corporate naming

--- resources/views/sidebar/sidebar.blade.php
+++ resources/views/sidebar/sidebar.blade.php
@@ -3,7 +3,7 @@
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
       
-      <span class="brand-text font-weight-light"><b>KRCS COOPERATE</b></span>
+      <span class="brand-text font-weight-light"><b>KRCS CORPORATE</b></span>
     </a>
 
     <!-- Sidebar -->
