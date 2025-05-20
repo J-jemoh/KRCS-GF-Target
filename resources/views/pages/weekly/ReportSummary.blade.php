20526794e187d7dda3e3414d4c9f49b7b89daeb6
JNyanga <44498304+J-jemoh@users.noreply.github.com>
2025-05-20 10:42:59 +0300

    updated report summary

--- resources/views/pages/weekly/ReportSummary.blade.php
+++ resources/views/pages/weekly/ReportSummary.blade.php
@@ -50,11 +50,11 @@
               <div class="accordion accordion-flush" id="accordionFlushExample">
 			  <div class="accordion-item">
 			    <h2 class="accordion-header">
-			      <button class="accordion-button collapsed bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+			      <button class="accordion-button collapsed bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne-{{$summary->region}}" aria-expanded="false" aria-controls="flush-collapseOne">
 			        {{$summary->region}} - Total Reports: </b>{{$summary->total_reports}}
 			      </button>
 			    </h2>
-			    <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+			    <div id="flush-collapseOne-{{$summary->region}}" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
 			      <div class="accordion-body">
 			      	<p><b>Summary of achievements</b></p>
 			      	<p>{!! $summary->achievements_summary!!}</</p>
